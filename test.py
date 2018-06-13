e5e3496230897a22046ebdaeb25c97a6e77de73c
Khalid <khld.163@gmail.com>
2018-06-13 14:54:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
 import cv2
-
-# Windows dependencies
-# - Python 2.7.6: http://www.python.org/download/
-# - OpenCV: http://opencv.org/
-# - Numpy -- get numpy from here because the official builds don't support x64:
-#   http://www.lfd.uci.edu/~gohlke/pythonlibs/#numpy
-
-# Mac Dependencies
-# - brew install python
-# - pip install numpy
-# - brew tap homebrew/science
-# - brew install opencv
+# - Python 3.5.2
 
 cap = cv2.VideoCapture(0)
 
